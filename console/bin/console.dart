743863c3f7311e9cdf4443508e433f2cffcd5d97
VitorSousa.2 <vitorgg1208bug@gmail.com>
2024-10-05 14:15:42 -0300

    primeiro commit

---
+++ console/bin/console.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+import 'package:console/console.dart' as console;
+
+void main(List<String> arguments) {
+  print('iai meu mano braga falo tu meu cria kkkkkkkkkk');
+  stdout.write("escreve ai teu nome meu mano\n");
+
+  String? nome = stdin.readLineSync();
+  stdout.write("bem vindo ${nome}");
+}
